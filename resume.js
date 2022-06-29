7506c9ce2e7f2e74f1ec5a126209d2bc6f139912
rmit-s3834830-Yating-Song <s3834830@student.rmit.edu.au>
2022-06-29 16:15:49 +1000

    css and resume change; add practice carousel

--- resume.js
+++ resume.js
@@ -11,3 +11,20 @@ $(".navbar-brand").click(function () {
 $(".nav-link").click(function () {
   $("#collapseNav").removeClass("show");
 });
+
+if ($("input").is(":checked")) {
+  var id = $("input:checked").attr("id");
+  console.log($("input:checked").attr("id"));
+  // $("div.item").eq()
+}
+$("div.item small").hide();
+var id = $("input:checked").attr("id");
+var newId = id - 1;
+$("div.item:eq(" + newId + ") small").show();
+
+function displayContent(radio) {
+  $("div.item small").hide();
+  var id = $("input:checked").attr("id");
+  var newId = id - 1;
+  $("div.item:eq(" + newId + ") small").show();
+}
